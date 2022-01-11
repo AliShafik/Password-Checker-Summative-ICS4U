3c70a2ddda9284dce5269490b8e95e062fe5c0f5
MALEK MEDHAT E. A. MEDHAT <92103350+Maleko5@users.noreply.github.com>
2022-01-11 14:55:37 +0000

    I added the Instructions and the code for user input

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("hi");
-  }
+    System.out.println("Welcome to password checker");
+    System.out.println("We take the password you input and check it's viability");
+    System.out.println("If the password is strong we will say the password is STRONG");
+    Scanner scnr = new Scanner(System.in);
+    System.out.println("If the password was not good we will say the password is weak & provide you with a strong password or how to make yours better");
+    System.out.println("Please enter your password");
+    String UI; // UI = User Input 
+    UI = scnr.nextLine();
+}
 }
