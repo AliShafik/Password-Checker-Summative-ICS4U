0f0d24a278be1c495d624510133f88c9510471aa
ALI SHAFIK <92734614+AliShafik@users.noreply.github.com>
2022-01-24 03:06:08 +0000

    Tried to create a GUI for the program using Java Swing

--- Main.java
+++ Main.java
@@ -1,13 +1,54 @@
+//Imports for java swing
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//Import for scanner for user input
 import java.util.Scanner;
-class Main {
+
+public class Main implements ActionListener {
+
+  private JFrame frame;
+  private JPanel mainMenuPanel;
+  private JPanel instructionsPanel;
+  private JButton instructionsButton;
+
+  public Main(){
+
+    frame = new JFrame();
+
+    instructionsButton = new JButton("Instructions");
+    instructionsButton.addActionListener(this);
+
+    mainMenuPanel = new JPanel();
+    mainMenuPanel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+    mainMenuPanel.setLayout(new GridLayout(0, 1));
+    mainMenuPanel.add(instructionsButton);
+
+    frame.add(mainMenuPanel, BorderLayout.CENTER);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setTitle("Password Maker and Creator");
+    frame.pack();
+    frame.setVisible(true);
+
+  }
+
   public static void main(String[] args) {
-    System.out.println("Welcome to password checker");
-    System.out.println("We take the password you input and check it's viability");
-    System.out.println("If the password is strong we will say the password is STRONG");
-    Scanner scnr = new Scanner(System.in);
-    System.out.println("If the password was not good we will say the password is weak & provide you with a strong password or how to make yours better");
-    System.out.println("Please enter your password");
-    String UI; // UI = User Input 
-    UI = scnr.nextLine();
+    new Main(); 
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e){
+
+  }
 }
-}
+
+
+
+
+
+
+/*char[] checker = new char[UI.length()];
+   for (int i = 0; i < UI.length(); i++) {
+      checker[i] = UI.charAt(i);
+  }*/
