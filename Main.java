ab5f0da1d4e06e8a436e95b9f8ec551fc360ea4b
MALEK MEDHAT E. A. MEDHAT <92103350+Maleko5@users.noreply.github.com>
2022-01-26 15:58:36 +0000

    I bulletproofed the code by adding an output to every possible input from the user

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+/*-----------------------------------------------------------
+PROGRAM DESCRIPTION:
+
+
+
+-----------------------------------------------------------*/
+
 //Imports for java swing
 import javax.swing.*;
 import java.awt.*;
@@ -5,16 +12,16 @@ import java.awt.event.*;
 
 class Main {
 
-  //Class for layout to manage panels and frame
+  // Class for layout to manage panels and frame
   public static class CLayout {
-    
-    //Class to retrieve main frame, main panel and card layout 
-    public class infoToRetreive {
-      private JPanel panelCont = new JPanel(); //main panel
-      private CardLayout cl = new CardLayout(); //card layout to manage all panels
-      private JFrame startFrame = new JFrame("Password Checker and Creator"); //main frame 
 
-      //Getters for main frame, main panel and card layout
+    // Class to retrieve main frame, main panel and card layout
+    public class infoToRetreive {
+      private JPanel panelCont = new JPanel(); // main panel
+      private CardLayout cl = new CardLayout(); // card layout to manage all panels
+      private JFrame startFrame = new JFrame("Password Checker and Creator"); // main frame
+
+      // Getters for main frame, main panel and card layout
       public CardLayout getcl() {
         return cl;
       }
@@ -30,18 +37,18 @@ class Main {
 
     public CLayout() {
 
-      //Retrieving info from class above to re-use
+      // Retrieving info from class above to re-use
       infoToRetreive infoRetreival = new infoToRetreive();
       JPanel panelcont = infoRetreival.getPanelCont();
       CardLayout cl = infoRetreival.getcl();
       JFrame startFrame = infoRetreival.getStartingFrame();
-      
-      //Setting main panel to follow the card layout
+
+      // Setting main panel to follow the card layout
       panelcont.setLayout(cl);
 
       // FRAME TO HOLD ALL PANELS
 
-      //Setting attributes of main frame
+      // Setting attributes of main frame
       startFrame.add(infoRetreival.getPanelCont());
       startFrame.setSize(450, 350);
       startFrame.setLocation(100, 100);
@@ -50,27 +57,27 @@ class Main {
 
       // MAIN MENU PANEL
 
-      //Declaring starting panel
+      // Declaring starting panel
       JPanel mainMenuPanel = new JPanel();
-      mainMenuPanel.setLayout(null); //setting layout to null to allow for movement of items
+      mainMenuPanel.setLayout(null); // setting layout to null to allow for movement of items
 
-      //Labels to display on starting label
-      JLabel programTitle = new JLabel("Password Checker and");//declaring label
-      programTitle.setFont(new Font("Verdana", Font.PLAIN, 35)); //setting font and font size of label
-      programTitle.setBounds(30, 70, 400, 50); //setting the location of label
-      mainMenuPanel.add(programTitle); //adding label to panel
+      // Labels to display on starting label
+      JLabel programTitle = new JLabel("Password Checker and");// declaring label
+      programTitle.setFont(new Font("Verdana", Font.PLAIN, 35)); // setting font and font size of label
+      programTitle.setBounds(30, 70, 400, 50); // setting the location of label
+      mainMenuPanel.add(programTitle); // adding label to panel
 
       JLabel programTitle2 = new JLabel("Creator");
       programTitle2.setFont(new Font("Verdana", Font.PLAIN, 35));
       programTitle2.setBounds(155, 120, 400, 50);
       mainMenuPanel.add(programTitle2);
 
-      JButton instructionsButton = new JButton("Instructions"); //declaring button
+      JButton instructionsButton = new JButton("Instructions"); // declaring button
       instructionsButton.setBounds(150, 200, 150, 25);
-      instructionsButton.setFocusable(false); //removing focus on button
+      instructionsButton.setFocusable(false); // removing focus on button
       mainMenuPanel.add(instructionsButton);
 
-      JButton checkerButton = new JButton("Checker"); 
+      JButton checkerButton = new JButton("Checker");
       checkerButton.setBounds(50, 250, 150, 25);
       checkerButton.setFocusable(false);
       mainMenuPanel.add(checkerButton);
@@ -80,110 +87,112 @@ class Main {
       creatorButton.setFocusable(false);
       mainMenuPanel.add(creatorButton);
 
-      //Action Listener to scan button action
+      // Action Listener to scan button action
       checkerButton.addActionListener(new ActionListener() {
         @Override
-        public void actionPerformed(ActionEvent arg0) { //checks for button click
-          cl.show(infoRetreival.getPanelCont(), "3"); //shows final panel on button click
+        public void actionPerformed(ActionEvent arg0) { // checks for button click
+          cl.show(infoRetreival.getPanelCont(), "3"); // shows final panel on button click
 
           PChecker checking = new PChecker();
-          checking.passwordchecker(); //activates password checker
-          
+          checking.passwordchecker(); // activates password checker
+
         }
       });
 
       creatorButton.addActionListener(new ActionListener() {
         @Override
-        public void actionPerformed(ActionEvent arg0) { //checks for button click
-          cl.show(infoRetreival.getPanelCont(), "3"); //shows final panel on button click
+        public void actionPerformed(ActionEvent arg0) { // checks for button click
+          cl.show(infoRetreival.getPanelCont(), "3"); // shows final panel on button click
         }
       });
 
       instructionsButton.addActionListener(new ActionListener() {
         @Override
-        public void actionPerformed(ActionEvent arg0) { //checks for button click
-          cl.show(infoRetreival.getPanelCont(), "2"); //shows instruction panel on button click
+        public void actionPerformed(ActionEvent arg0) { // checks for button click
+          cl.show(infoRetreival.getPanelCont(), "2"); // shows instruction panel on button click
         }
       });
 
       // INSTRUCTIONS PANEL
 
       JPanel instructionsPanel = new JPanel();
-      instructionsPanel.setLayout(null); //set layout to null to allow movement of attributes
+      instructionsPanel.setLayout(null); // set layout to null to allow movement of attributes
 
       JLabel instruction1 = new JLabel("1. Click either Checker or Creator.");
       instruction1.setFont(new Font("Verdana", Font.PLAIN, 15));
       instruction1.setBounds(25, 40, 400, 100);
       instructionsPanel.add(instruction1);
 
-      JLabel instruction2 = new JLabel("<html>2. Checker will ask for your password and return<br/>how good it is to you and what you are missing.</html>");
-      instruction2.setFont(new Font("Verdana", Font.PLAIN, 15)); //<html> is used to create a new line 
+      JLabel instruction2 = new JLabel(
+          "<html>2. Checker will ask for your password and return<br/>how good it is to you and what you are missing.</html>");
+      instruction2.setFont(new Font("Verdana", Font.PLAIN, 15)); // <html> is used to create a new line
       instruction2.setBounds(25, 90, 400, 100);
       instructionsPanel.add(instruction2);
 
-      JLabel instruction3 = new JLabel("<html>3. Creator will create a password for you randomly<br/>or create one based off of certain questions.</html>");
+      JLabel instruction3 = new JLabel(
+          "<html>3. Creator will create a password for you randomly<br/>or create one based off of certain questions.</html>");
       instruction3.setFont(new Font("Verdana", Font.PLAIN, 15));
       instruction3.setBounds(25, 150, 400, 100);
       instructionsPanel.add(instruction3);
 
-      JButton backButton = new JButton("Back"); //declaring button
+      JButton backButton = new JButton("Back"); // declaring button
       backButton.setBounds(150, 280, 150, 25);
-      backButton.setFocusable(false); //removing focus on button
+      backButton.setFocusable(false); // removing focus on button
       instructionsPanel.add(backButton);
 
       backButton.addActionListener(new ActionListener() {
         @Override
-        public void actionPerformed(ActionEvent arg0) { //checks for button click
-          cl.show(infoRetreival.getPanelCont(), "1"); //shows main menu panel on button click
+        public void actionPerformed(ActionEvent arg0) { // checks for button click
+          cl.show(infoRetreival.getPanelCont(), "1"); // shows main menu panel on button click
         }
       });
 
       // FINAL PANEL
 
-      JPanel finalPanel = new JPanel(); //creating final panel
+      JPanel finalPanel = new JPanel(); // creating final panel
       finalPanel.setLayout(null);
 
-      //Labels to display on final panel
-      JLabel restartLabel = new JLabel("Restart?"); 
+      // Labels to display on final panel
+      JLabel restartLabel = new JLabel("Restart?");
       restartLabel.setFont(new Font("Verdana", Font.PLAIN, 25));
       restartLabel.setBounds(170, 50, 400, 100);
       finalPanel.add(restartLabel);
 
-      JButton quitButton = new JButton("Quit"); //quit button to exit program
+      JButton quitButton = new JButton("Quit"); // quit button to exit program
       quitButton.setFocusable(false);
       quitButton.setBounds(250, 200, 150, 25);
       finalPanel.add(quitButton);
 
-      JButton restartButton = new JButton("Restart"); //play again button to reset program
+      JButton restartButton = new JButton("Restart"); // play again button to reset program
       restartButton.setFocusable(false);
       restartButton.setBounds(50, 200, 150, 25);
       finalPanel.add(restartButton);
 
-      quitButton.addActionListener(new ActionListener() { //action listener to see if quit button is clicked
+      quitButton.addActionListener(new ActionListener() { // action listener to see if quit button is clicked
         @Override
         public void actionPerformed(ActionEvent e) {
-          System.exit(0); //closes program
+          System.exit(0); // closes program
         }
       });
 
-      restartButton.addActionListener(new ActionListener() { //action listener to see if restart button is clicked
+      restartButton.addActionListener(new ActionListener() { // action listener to see if restart button is clicked
         @Override
         public void actionPerformed(ActionEvent e) {
-          new CLayout(); //starts a new program from the start
+          new CLayout(); // starts a new program from the start
         }
       });
 
       // CONT PANEL ADJUSTMENTS
 
-      infoRetreival.getPanelCont().add(mainMenuPanel, "1"); //adds starting panel to main panel
-      infoRetreival.getPanelCont().add(instructionsPanel, "2"); //adds instructions panel to main panel
-      infoRetreival.getPanelCont().add(finalPanel, "3"); //adds final panel to main panel
-      cl.show(infoRetreival.getPanelCont(), "1"); //shows first panel on program start
+      infoRetreival.getPanelCont().add(mainMenuPanel, "1"); // adds starting panel to main panel
+      infoRetreival.getPanelCont().add(instructionsPanel, "2"); // adds instructions panel to main panel
+      infoRetreival.getPanelCont().add(finalPanel, "3"); // adds final panel to main panel
+      cl.show(infoRetreival.getPanelCont(), "1"); // shows first panel on program start
 
     }
   }
 
   public static void main(String[] args) {
-    new CLayout(); //calls entire program to start
+    new CLayout(); // calls entire program to start
   }
 }
--- PChecker.java
+++ PChecker.java
@@ -1,74 +1,101 @@
 import java.util.Scanner;
-public class PChecker{
- public static void passwordchecker(){
-    int minLength = 8; //minimum length for password
+
+public class PChecker {
+  public static void passwordchecker() {
+    int minLength = 8; // minimum length for password
     int maxLength = 16;// maximum Length for password
     int digit = 0;// amount of numbers in password
     int special = 0;// amount of special characters in password
-    int lowCase = 0; // amount of lower cases in password 
-    int uppCase = 0;//amount of upper cases in password
-    
+    int lowCase = 0; // amount of lower cases in password
+    int uppCase = 0;// amount of upper cases in password
+
     Scanner scnr = new Scanner(System.in);
     System.out.println("Please enter your password: ");
     String password = scnr.nextLine(); // detects user input for a password string
 
-      if (password.length() > minLength && password.length()<= maxLength) {
-      for (int i = 0; i< password.length();i++) {
+    if (password.length() > minLength && password.length() <= maxLength) {
+      for (int i = 0; i < password.length(); i++) {
         char checker = password.charAt(i);
-        if (Character.isUpperCase(checker)){
-          uppCase++; //determines the number of upper case characters in the user input
+        if (Character.isUpperCase(checker)) {
+          uppCase++; // determines the number of upper case characters in the user input
         }
-        if (Character.isLowerCase(checker)){
-          lowCase++; //determines the number of lower case characters in the user input
+        if (Character.isLowerCase(checker)) {
+          lowCase++; // determines the number of lower case characters in the user input
         }
-        if (Character.isDigit(checker)){
-          digit++; //determines the number of integers characters in the user input
+        if (Character.isDigit(checker)) {
+          digit++; // determines the number of integers characters in the user input
         }
-        if (checker == 33 || checker == 63 ){
-          special++; //determines the number of special characters in the user input
-        }
-      }
-        if (special>=1&&lowCase>=1&&uppCase>=1&&digit>=1){
-          System.out.println("The password is PERFECT");
-        }// checks that all aspects of a good password is met and outputs if the password is good if not the rest of the code will run
-      }
-      if (password.length() < minLength){
-        for (int i=0;i<password.length();i++){
-          char c = password.charAt(i);// checks characters in the string
-          if (Character.isLowerCase(c)){
-            lowCase++;
-          }
-        }
-        if (lowCase>0){//outputs password requirment if password only has lower case characters
-          System.out.println("your password must have " + minLength + " characters and a maximum of " + maxLength + " characters");
-          System.out.println("You need at least 1 uppercase character, 1 digit and 1 special character");
+        if (checker == 33 || checker == 63) {
+          special++; // determines the number of special characters in the user input
         }
       }
-      else if (password.length()<minLength&&uppCase>1){
-        for (int i=0;i<password.length();i++){
-          char checker = password.charAt(i);
-          if (Character.isLowerCase(checker)){
-            lowCase++;
-          }
-          if (Character.isUpperCase(checker)){
-            uppCase++;
-          }
-        }
-        if (lowCase>0&&uppCase>0){//outputs password requirment if password only has lower & upper case characters
-          System.out.println("your password must have " + minLength + " characters");
-           System.out.println("You need at least 1 digit and 1 special character");
-        }
+    }
+    if (special >= 1 && lowCase >= 1 && uppCase >= 1 && digit >= 1) {
+      System.out.println("The password is PERFECT");
+    } // checks that all aspects of a good password is met and outputs if the password
+      // is good if not the rest of the code will run
+
+    else if (lowCase>0&&password.length()>minLength && password.length()<=maxLength) {// outputs password requirment if password only has lower case characters
+      System.out.println("You need at least 1 uppercase character, 1 digit and 1 special character");
+    }
+
+    else if (uppCase>0&&password.length()>minLength && password.length()<=maxLength) {// outputs password requirment if password only has upper case characters
+      System.out.println("You need at least, 1 digit and 1 special character, lower case characters");
+    }
+    else if (digit>0&&password.length()>minLength && password.length()<=maxLength) {// outputs password requirment if password only has upper case characters
+      System.out.println("You need at least 1 uppercase character, 1 lower case character and 1 special character");
+    }
+    else if (special>0&&password.length()>minLength && password.length()<=maxLength) {// outputs password requirment if password only has upper case characters
+      System.out.println("You need at least 1 uppercase character, 1 lower case character and 1 digit character");
+    }
+
+    else if (password.length() > minLength && password.length() <= maxLength && lowCase > 0 && uppCase > 0 && digit > 0
+          && special == 0) {// outputs password requirment if password is missing a special characters
+        System.out.println("You need at least 1 special character");
       }
-        if (password.length()>maxLength||password.length()>=maxLength&&uppCase>0&&lowCase>0&&digit>0){//outputs password requirment if password has most requirments but it's too long
-           System.out.println("Password is too long. Limit is " + maxLength + " characters");
-           System.out.println("Need at least 1 special characters");
+    else if (password.length() > minLength && password.length()<= maxLength && lowCase > 0 && uppCase > 0 && digit > 0
+          && special == 0) {// outputs password requirment if password is missing a special character and a digit
+        System.out.println("You need special characters in your password");
+      }
+
+
+    else if (password.length() > minLength && password.length() < maxLength && lowCase > 0 && uppCase > 0 && digit == 0&& special > 0) {// outputs password requirment if password is missing a special character and a digit
+        System.out.println("You need digits in your password");
         }
-     if (password.length()>minLength&&password.length()<maxLength&&lowCase>0&&uppCase>0&&digit>0&&special==0){//outputs password requirment if password is missing a special characters
-       System.out.println("You need at least 1 special character");
-     }
-     if (password.length()>minLength&&password.length()<maxLength&&lowCase>0&&uppCase>0&&digit==0&& special==0){//outputs password requirment if password is missing a special character and a digit
-        System.out.println("You need at least 1 special character & at least 1 digit");
-     }
+    else if (password.length() > minLength && password.length() < maxLength && lowCase == 0 && uppCase > 0 && digit > 0&& special > 0) {// outputs password requirment if password is missing a special character and a digit
+        System.out.println("You need lower cases in your password");
+        }
+    else if (password.length() > minLength && password.length() < maxLength && lowCase > 0 && uppCase == 0 && digit > 0&& special > 0) {// outputs password requirment if password is missing a special character and a digit
+    System.out.println("You need Upper cases in your password");
+    }
+    else if (password.length() > minLength && password.length() < maxLength && lowCase > 0 && uppCase == 0 && digit > 0&& special > 0) {// outputs password requirment if password is missing a special character and a digit
+    System.out.println("You need Upper cases in your password");
+    }
+    else if (password.length() > minLength && password.length() < maxLength && lowCase == 0 && uppCase == 0 && digit > 0&& special > 0) {// outputs password requirment if password is missing a special character and a digit
+    System.out.println("You need Upper & lower cases in your password");
+    }
+    else if (password.length() > minLength && password.length() < maxLength && lowCase == 0 && uppCase > 0 && digit == 0&& special > 0) {// outputs password requirment if password is missing a special character and a digit
+    System.out.println("You need lower cases & digits in your password");
+    }
+    else if (password.length() > minLength && password.length() < maxLength && lowCase == 0 && uppCase > 0 && digit > 0&& special == 0) {// outputs password requirment if password is missing a special character and a digit
+    System.out.println("You need lower cases & special characters in your password");
+    }
+    else if (password.length() > minLength && password.length() < maxLength && lowCase > 0 && uppCase == 0 && digit == 0&& special > 0) {// outputs password requirment if password is missing a special character and a digit
+    System.out.println("You need Upper cases & digits in your password");
+    }
+    else if (password.length() > minLength && password.length() < maxLength && lowCase > 0 && uppCase == 0 && digit > 0&& special == 0) {// outputs password requirment if password is missing a special character and a digit
+    System.out.println("You need Upper cases & special in your password");
+    }
+    else if (password.length() > minLength && password.length() < maxLength && lowCase > 0 && uppCase > 0 && digit == 0&& special == 0) {// outputs password requirment if password is missing a special character and a digit
+    System.out.println("You need digits & special characters in your password");
+    }
+
+    if (password.length() < minLength) {
+      System.out.println("your password must have " + minLength + " characters and a maximum of " + maxLength + " characters");
+      }
      
+      if (password.length() > maxLength) {// outputs password requirment if password has most requirments but it's too long
+        System.out.println("your password must have " + minLength + " characters and a maximum of " + maxLength + " characters");
+      }
+      }
   }
-}
